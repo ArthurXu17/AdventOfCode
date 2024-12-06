ec307c66ce740d3fad5502975061db13b871e034
Arthur Xu <arthuryangxu100@gmail.com>
2024-12-06 10:48:51 -0500

    Gold wrong attempt 2, using loop length detection

--- 2024/Day6/gold.cc
+++ 2024/Day6/gold.cc
@@ -22,6 +22,46 @@ bool goesOutOfBounds(int x, int y, int dx, int dy, int m, int n) {
     return x + dx < 0 || x + dx >= n || y + dy < 0 || y+dy >= m;
 }
 
+bool isLoop(int x, int y, int dx, int dy, int m, int n, vector<string>& grid) {
+    int x0 = x;
+    int y0 = y;
+    int dx0 = dx;
+    int dy0 = dy;
+    int numTurns = 0;
+    int firstStraightLength = -1;
+    int reverseStraightLength = -1;
+    int curLength = 0;
+    while (true) {
+        if (goesOutOfBounds(x,y,dx,dy,m,n)) {
+            break;
+        }
+        if (grid[x+dx][y+dy] == '#') {
+            changeDirection(dx,dy);
+            if (numTurns == 0) {
+                numTurns++;
+                firstStraightLength = curLength;
+                curLength = 0;
+            } else if (numTurns == 1) {
+                numTurns++;
+                curLength = 0;
+            } else if (numTurns == 2) {
+                reverseStraightLength = curLength;
+                if (firstStraightLength == reverseStraightLength) {
+                    cout<<"Found solution"<<endl;
+                    cout<<"Initial position: "<<x0<<", "<<y0<<endl;
+                    cout<<"Initial direction: "<<dx0<<", "<<dy0<<endl;
+                    cout<<"Straight Length: "<<firstStraightLength<<endl;
+                }
+                return firstStraightLength == reverseStraightLength;
+            }
+        }
+        curLength++;
+        x += dx;
+        y += dy;
+    }
+    return false;
+}
+
 int main() {
     string str;
     vector<string> grid;
@@ -43,24 +83,24 @@ int main() {
     int n = grid.size();
     int m = grid[0].size();
     //cout<<n<<" x "<< m<<endl;
-    vector<vector<set<pair<int, int>>>> previousDirections(n, vector<set<pair<int, int>>>(m, set<pair<int, int>>())); // for each location, store the directions that we've been at that location
     int dx = -1;
     int dy = 0;
     int result = 0;
     while (true) {
-        //cout<<x<<", "<<y<<endl;
-        previousDirections[x][y].insert(make_pair(dx,dy));
         if (goesOutOfBounds(x,y,dx,dy,m,n)) {
             break;
         }
-        int new_dx = dx;
-        int new_dy = dy;
-        changeDirection(new_dx, new_dy);
-        if (previousDirections[x][y].find(make_pair(new_dx, new_dy)) != previousDirections[x][y].end()) {
-            result++;
-        }
+        
         if (grid[x+dx][y+dy] == '#') {
             changeDirection(dx,dy);
+        } else {
+            int new_dx = dx;
+            int new_dy = dy;
+            changeDirection(new_dx, new_dy);
+            if (isLoop(x, y, new_dx, new_dy, m, n, grid)) {
+                cout<<"O placement: "<<x+dx<<", "<<y+dy<<endl;
+                result++;
+            }
         }
         x += dx;
         y += dy;
