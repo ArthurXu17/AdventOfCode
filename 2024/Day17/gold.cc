3d5f72e57edd99b0e0159cacd89c1d0aec9ae211
Arthur Xu <arthuryangxu100@gmail.com>
2024-12-17 20:27:33 -0500

    Cleaning up Gold code

--- 2024/Day17/gold.cc
+++ 2024/Day17/gold.cc
@@ -15,9 +15,7 @@ typedef unsigned long long ll;
 
 int main() {
     vector<int> overallGoal = {2,4,1,5,7,5,4,3,1,6,0,3,5,5,3,0};
-    assert(overallGoal.size() == 16);
-    int count = 0;
-    int t = 3;
+
     ll value = 0;
     deque<int> goal;
     for (int i = 1; i <= 16; i++) {
@@ -51,59 +49,5 @@ int main() {
             }
         }
     }
-    
-    ll a = 845878194359504;
-    ll b = 0; 
-    ll c = 0;
-    vector<int> sanityResult;
-    while (a != 0) {
-        b = a % 8; // 2 4
-        int temp = b ^ 5; // 1 5
-        c = a / pow(2, temp); // 7 5
-        b = b ^ c; //  4 3
-        b = b ^ 3; // 1 6
-        a = a / 8;// 0 3
-        sanityResult.push_back(b % 8);
-    }
-    for (auto & n : sanityResult) {
-            cout<<n<<" ";
-        }
-        cout<<endl;
-        cout<<"Sanity Result Size: "<<sanityResult.size()<<endl;
-    /*
-    for (ll start = 24; start <= 31; start++) {
-        count++;
-        if (count % 100000 == 0) {
-            cout<<count<<endl;
-        }
-        ll a = start;
-        ll b = 0; 
-        ll c = 0;
-        vector<int> result;
-        while (a != 0) {
-            b = a % 8; // 2 4
-            int temp = b ^ 5; // 1 5
-            c = a / pow(2, temp); // 7 5
-            b = b ^ c; //  4 3
-            b = b ^ 3; // 1 6
-            a = a / 8;// 0 3
-            result.push_back(b % 8);
-        }
-        cout<<start<<": ";
-        for (auto & n : result) {
-            cout<<n<<" ";
-        }
-        cout<<endl;
-        
-        if (result == goal) {
-            cout<<"Start Value: "<<start<<endl;
-            break;
-        }
-    }*/
-    /*
-    for (int i = 0;  i < result.size(); i++) {
-        cout<<i + 1<<": "<<result[i]<<endl;
-    }
-    cout<<endl;
-    cout<<"Num elements: "<< result.size()<<endl;*/
+
 }
